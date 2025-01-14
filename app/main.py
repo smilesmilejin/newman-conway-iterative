73068eaa4df3cb9c2704bf943cd1b95b0c66b164
Adrian Prado <adrian.prado@adadevelopersacademy.org>
2025-01-14 15:32:56 -0600

    Added solution and __init__.py files

--- app/main.py
+++ app/main.py
@@ -1,2 +1,11 @@
 def newman_conway(n):
-    pass
+    if n < 1:
+        return []
+
+    memo = [0, 1, 1]
+    count = 3
+    while count <= n:
+        memo.append(memo[memo[count - 1]] + memo[count - memo[count - 1]])
+        count += 1
+
+    return memo[1:n+1]
